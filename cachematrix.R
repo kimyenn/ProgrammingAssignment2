a84df279d6769191d445bcb940bc90b225792a13
kimyenn <knguyen07@gmail.com>
2016-04-03 18:06:21 -0700

    amended makeCacheMatrix typo

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setinverse <- function(inverse) inv <<- inverse
   getinverse <- function() inv
-  list(set = set, get = getinverse, setinverse = setinverse, getinverse = getinverse)
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
